7d4334441c75275907e855ac2d5c8f3429af9336
martingiufrida <giufridamartin@gmail.com>
2026-05-12 18:46:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 import plotly.graph_objects as go
-import yfinance as yf  # <--- ESTA ES LA NUEVA LÍNEA
+import yfinance as yf
+import requests  # <--- ESTA ES LA NUEVA LÍNEA
 
 # 1. CONFIGURACIÓN INICIAL
 st.set_page_config(page_title="Agro-Terminal Pro", layout="wide", initial_sidebar_state="expanded")
